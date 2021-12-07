a87828fac4b504193b2c2483bd95f6a5175d28b3
Md. Mohiuddin Khan <mohiuddin.ewu@gmail.com>
2021-12-07 16:06:02 +0600

    Updated

--- src/RandomQuote.php
+++ src/RandomQuote.php
@@ -2,20 +2,20 @@
 namespace RandomQuote;
 
 class RamdonQuote{
-    private $quote = '';
-
     private function getRandomQuotes()
     {
         //Get the list of quotes.
         $quotes = file_get_contents(__DIR__ . '/../inc/quotes.json');
-    
+
         //Convert JSON document to PHP  array.
         $quotes = json_decode($quotes, true);
+        $quotes = $quotes['quotes'];
+
         //Get a random index number.
         $index = mt_rand(0, count( $quotes ) -1 );
 
         //Return the random quote.
-        return $quotes['quotes'][$index];
+        return $quotes[$index];
     }
 
     public function geenerate(){
